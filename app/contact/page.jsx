0d704af4165d816e665d4a210e5391944b120fe5
ABDOOMARI11 <112636088+ABDOOMARI11@users.noreply.github.com>
2024-07-22 22:26:14 +0100

    added v1

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -13,6 +13,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const info = [
   {
@@ -31,8 +32,6 @@ const info = [
     description: "Ben Guerir 43150, Morocco",
   },
 ];
-import { motion } from "framer-motion";
-import { SelectPortal } from "@radix-ui/react-select";
 
 const Contact = () => {
   return (
@@ -40,24 +39,26 @@ const Contact = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ delay: 2.4, duration: 0.4, ease: "easeIn" }}
-      className="py-6"
+      className="py-6 px-4 sm:px-6 lg:px-8"
     >
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-[30px]">
-          <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">let&apos;s work together</h3>
-              <p className=" text-white/60">
+        <div className="flex flex-col gap-6 xl:flex-row xl:gap-[30px]">
+          <div className="flex flex-col xl:w-[54%] order-2 xl:order-none">
+            <form className="flex flex-col p-6 bg-[#27272c] rounded-xl">
+              <h3 className="text-2xl md:text-3xl lg:text-4xl text-accent">
+                Let's work together
+              </h3>
+              <p className="text-sm md:text-base lg:text-lg text-white/60">
                 To create innovative solutions and achieve extraordinary
                 results.
               </p>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
+              <div className="grid grid-cols-1 gap-4 md:grid-cols-2 gap-6 mt-4">
                 <Input type="firstname" placeholder="Firstname" />
                 <Input type="lastname" placeholder="Lastname" />
                 <Input type="email" placeholder="Email address" />
                 <Input type="phone" placeholder="Phone number" />
               </div>
-              <Select>
+              <Select className="mt-4">
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Select a service" />
                 </SelectTrigger>
@@ -72,29 +73,33 @@ const Contact = () => {
                 </SelectContent>
               </Select>
               <Textarea
-                className="h-[200px]"
-                placeholder="Type ypur message here"
+                className="h-[150px] md:h-[200px] mt-4"
+                placeholder="Type your message here"
               />
-              <Button size="md" className="max-w-40 mt-3">
+              <Button size="md" className="max-w-xs mt-4">
                 Send message
               </Button>
             </form>
           </div>
-          <div className="flex flex-1 items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            <ul className="flex flex-col gap-10">
-              {info.map((item, index) => {
-                return (
-                  <li key={index} className="flex  items-center gap-6">
-                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
-                      <div className="text-[28px]">{item.icon}</div>
+          <div className="flex flex-1 items-center xl:justify-end xl:w-[46%] order-1 xl:order-none">
+            <ul className="flex flex-col gap-6">
+              {info.map((item, index) => (
+                <li key={index} className="flex items-center gap-4 sm:gap-6">
+                  <div className="w-[40px] h-[40px] sm:w-[52px] sm:h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                    <div className="text-[20px] sm:text-[24px] xl:text-[28px]">
+                      {item.icon}
                     </div>
-                    <div className="flex-1">
-                      <p className="text-white/60">{item.title}</p>
-                      <h3 className="text-xl">{item.description}</h3>
-                    </div>
-                  </li>
-                );
-              })}
+                  </div>
+                  <div className="flex-1">
+                    <p className="text-sm sm:text-base text-white/60">
+                      {item.title}
+                    </p>
+                    <h3 className="text-[12px] sm:text-[12px] xl:text-xl">
+                      {item.description}
+                    </h3>
+                  </div>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
