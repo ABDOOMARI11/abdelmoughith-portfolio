46aaa1f449942dfa99a200c52277a47ccc4c7436
ABDOOMARI11 <112636088+ABDOOMARI11@users.noreply.github.com>
2024-07-23 19:35:00 +0100

    added v1

--- app/page.jsx
+++ app/page.jsx
@@ -8,16 +8,16 @@ const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full px-4">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-4 xl:pb-20 pt-8 pb-12">
-          <div className="text-center xl:text-left order-2 xl:order-none mb-6 xl:mb-0">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-4 xl:pb-20 pt-6 pb-10">
+          <div className="text-center xl:text-left order-2 xl:order-none mb-3 xl:mb-0">
             <span className="block text-sm xl:text-base text-gray-500">
               Software Developer
             </span>
-            <h1 className="text-3xl xl:text-5xl font-bold mb-4">
+            <h1 className="text-3xl xl:text-5xl font-bold mb-2">
               Hello I&apos;m <br />
               <span className="text-accent">El Aoumari Abdelmoughith</span>
             </h1>
-            <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 text-white/80 text-sm xl:text-base">
+            <p className="max-w-[500px] mx-auto xl:mx-0 mb-4 text-white/80 text-sm xl:text-base">
               I excel at crafting elegant digital experiences and I am
               proficient in various programming languages and technologies.
             </p>
@@ -43,7 +43,7 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="order-1 xl:order-none mb-6 xl:mb-0">
+          <div className="order-1 xl:order-none mb-2 xl:mb-0">
             <Photo />
           </div>
         </div>
