1a8aef57891793cfae32a2879a59f97524722bd3
ABDOOMARI11 <112636088+ABDOOMARI11@users.noreply.github.com>
2024-07-23 11:57:27 +0100

    added v1

--- app/page.jsx
+++ app/page.jsx
@@ -10,11 +10,11 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           <div className="text-center xl:text-left order-2 xl:order-none ">
             <span>Software Developer</span>
-            <h1 className="h1 mb-6">
+            <h1 className="h1 mb-4">
               Hello I&apos;m <br />
               <span className="text-accent">El Aoumari Abdelmoughith</span>
             </h1>
-            <p className="max-w-[500px] mb-9 text-white/80">
+            <p className="max-w-[500px] mb-6 text-white/80">
               I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
@@ -29,7 +29,7 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="order-1 xl:order-none  mb-8  xl:mb-0">
+          <div className="order-1 xl:order-none  mb-4  xl:mb-0">
             <Photo />
           </div>
         </div>
